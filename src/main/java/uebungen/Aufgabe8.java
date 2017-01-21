cc9e812d8eaf415da94430566b930858ac273b81
Mark Keinhörster <mark.keinhoerster@gmail.com>
2017-01-21 13:25:50 +0100

    Aufgabe8 hinzugefuegt

---
+++ src/main/java/uebungen/Aufgabe8.java
@@ -0,0 +1,19 @@
+package uebungen;
+import tastatur.Tastatur;
+
+
+public class Aufgabe8 {
+
+	public static void main(String args[]) {
+		System.out.print("Anzahl eingeben: ");
+		int anzahl = Tastatur.readInteger();
+		System.out.println();
+		while (anzahl > 0) {
+			System.out.print("Ziehe eine Zahl: ");
+			anzahl -= Tastatur.readInteger();
+			System.out.println("Verbleibend: " + anzahl);
+			System.out.println();
+		}
+		System.out.println("gewonnen.");
+	}
+}
